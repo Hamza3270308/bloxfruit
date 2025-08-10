7697c2240739ff15b52b1149bd5b86d85a6ad3d3
Hamza3270308 <hami5511@gmail.com>
2025-08-10 15:05:49 +0500

    05. MukuroHub.lua

--- 05. MukuroHub.lua
+++ 05. MukuroHub.lua
@@ -119,7 +119,7 @@ end
 -- Run the script after key verification
 local function runMainScript()
     -- Your original script logic here
-    local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
+    local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Scripts/refs/heads/main/main.luau"))(Settings)
 
     for PlaceID, Execute in pairs(Games) do
         if PlaceID == game.PlaceId then
